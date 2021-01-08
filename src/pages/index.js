bccd8feb671eb8aa5d479309b1a6f1b7de4b110a
Sashankr <sashankr97@gmail.com>
2021-01-08 19:38:03 +0530

    Worked on learning Gatsby Images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,34 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link,graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+import Img from 'gatsby-image';
+
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <Img fluid={data.airportGatsbyImage.childImageSharp.fluid} alt="airport image"/>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const query =  graphql`
+
+query{
+  airportGatsbyImage : file(relativePath: {eq: "airport.jpg"}) {
+    childImageSharp {
+      fluid(maxWidth: 2000) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}
+`
